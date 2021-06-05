fb31b844d3f5c95f4ea30f51c57775446badfccf
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-05 00:50:24 -0300

    chore: add NovaSerie event listeners

---
+++ app/Listeners/EnviarEmailNovaSerieCadastrada.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\NovaSerie;
+use App\User;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class EnviarEmailNovaSerieCadastrada
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  NovaSerie  $event
+     * @return void
+     */
+    public function handle(NovaSerie $event)
+    {
+        $users = User::all();
+        foreach ($users as $i => $user) {
+            $email = new \App\Mail\NovaSerie(
+                $event->nomeSerie,
+                $event->qtdTemporadas,
+                $event->qtdEpisodios
+            );
+
+            $email->subject = 'Nova Serie Adicionada!';
+
+            $multiplicador = $i + 1;
+            $quando = now()->addSecond($multiplicador * 10);
+            \Illuminate\Support\Facades\Mail::to($user)->later(
+                $quando,
+                $email
+            );
+        }
+    }
+}
