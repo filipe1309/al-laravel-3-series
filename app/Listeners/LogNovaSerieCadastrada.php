3fee3f3a89988271a4ea839ac2d88680f626c4b6
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-05 00:57:56 -0300

    refactor: make listeners async

--- app/Listeners/LogNovaSerieCadastrada.php
+++ app/Listeners/LogNovaSerieCadastrada.php
@@ -7,7 +7,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Log;
 
-class LogNovaSerieCadastrada
+class LogNovaSerieCadastrada implements ShouldQueue
 {
     /**
      * Create the event listener.
